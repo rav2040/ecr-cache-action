6d9838e4bd0d22d3eaf64adfed926d0069e23d16
Stuart Kennedy <26479125+rav2040@users.noreply.github.com>
2023-04-04 08:47:08 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -7,41 +7,55 @@ main();
 async function main() {
     try {
         const repositoryUrl = core.getInput("repository-url");
+        const password = execFileSync("aws", ["ecr", "get-login-password"]);
+
         const runId = core.getInput("run-id");
-        const imageTag = core.getInput("image-tag") ?? "";
-        const imageLabel = core.getInput("image-label") ?? "";
 
-        if (imageTag.length === 0 && imageLabel.length === 0) {
-            throw Error("At least one of 'image-tag' or 'image-label' must be defined.")
-        }
+        if (runId === undefined) {
+            const imageTag = core.getInput("image-tag") ?? "";
+            const imageLabel = core.getInput("image-label") ?? "";
 
-        const imageLabelTag = execFileSync("docker",
-            [
+            if (imageTag.length === 0 && imageLabel.length === 0) {
+                throw Error("At least one of 'image-tag' or 'image-label' must be defined.")
+            }
+
+            const imageLabelTag = execFileSync("docker", [
                 "images",
                 "--filter", `label=${imageLabel.trim()}`,
                 "--format", '"{{.Repository}}"'
-            ],
-            { encoding: "utf8" }
-        )
+            ], { encoding: "utf8" });
 
-        const tags = [imageTag, imageLabelTag].join("\n").split("\n").map((str) => str.trim()).filter(Boolean);
-        const uniqueTags = Array.from(new Set(tags));
+            const tags = [imageTag, imageLabelTag].join("\n").split("\n").map((str) => str.trim()).filter(Boolean);
+            const uniqueTags = Array.from(new Set(tags));
 
-        const password = execFileSync("aws", ["ecr", "get-login-password"]);
-        execFileSync("docker", ["login", "--username", "AWS", "--password-stdin", repositoryUrl], { input: password });
+            execFileSync("docker", ["login", "--username", "AWS", "--password-stdin", repositoryUrl], { input: password });
 
-        const cb = runId === undefined ? (tag) => {
-            const remoteTag = `${github.context.runId}-${tag}`;
-            execFileSync("docker", ["tag", tag, `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
-            execFileSync("docker", ["push", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
-        } : (tag) => {
-            const remoteTag = `${runId}-${tag}`;
-            execFileSync("docker", ["pull", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
-            execFileSync("docker", ["tag", `${repositoryUrl}:${remoteTag}`, tag], { stdio: "inherit" });
-            execFileSync("docker", ["rmi", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+            uniqueTags.forEach((tag) => {
+                const remoteTag = `${github.context.runId}-${tag}`;
+                execFileSync("docker", ["tag", tag, `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+                execFileSync("docker", ["push", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+            });
+
+            execFileSync("docker", ["logout", repositoryUrl]);
+            return;
         }
 
-        uniqueTags.forEach(cb);
+        const output = execFileSync("aws", [
+            "ecr", "list-images",
+            "--repository-name", repositoryUrl.slice(repositoryUrl.lastIndexOf("/") + 1),
+            "--filter", '"tagStatus=TAGGED"',
+            "--query", `'imageIds[?starts_with(imageTag, \`${runId}-\`)]'`,
+            "--no-paginate",
+        ]);
+        const tags = JSON.parse(output).map(({ imageTag }) => imageTag);
+
+        execFileSync("docker", ["login", "--username", "AWS", "--password-stdin", repositoryUrl], { input: password });
+
+        tags.forEach((tag) => {
+            execFileSync("docker", ["pull", `${repositoryUrl}:${tag}`], { stdio: "inherit" });
+            execFileSync("docker", ["tag", `${repositoryUrl}:${tag}`, tag.slice(tag.indexOf("-") + 1)], { stdio: "inherit" });
+            execFileSync("docker", ["rmi", `${repositoryUrl}:${tag}`], { stdio: "inherit" });
+        });
 
         execFileSync("docker", ["logout", repositoryUrl]);
     } catch (error) {

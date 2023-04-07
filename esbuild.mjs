18c8e4e38e2c19f86139ba69174524c715b6d25f
Stuart Kennedy <rav2040@gmail.com>
2023-04-07 13:47:35 +1200

    Initial commit

--- esbuild.mjs
+++ esbuild.mjs
@@ -4,17 +4,16 @@ const config = {
     bundle: true,
     minify: true,
     charset: "utf8",
-    entryPoints: ["index.js"],
-    outdir: "dist",
+    entryPoints: ["src/main.ts"],
+    outfile: "lib/main.mjs",
     platform: "node",
     target: "node16",
+    format: "esm",
     mainFields: ["module", "main"],
 };
 
 build(config)
-    .then(() => {
-        console.log("Build completed successfully.");
-    })
+    .then(() => console.info("Build completed successfully."))
     .catch((err) => {
         console.error(err);
         process.exit(1);
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { execFileSync } from "child_process";
+import { getInput, setFailed } from "@actions/core";
+import { context as gitHubContext } from "@actions/github";
+
+async function main() {
+    try {
+        const repositoryUrl = getInput("repository-url");
+        const imageTag = getInput("image-tag");
+        const runNumber = getInput("run-number") || null;
+
+        const password = execFileSync("aws", ["ecr", "get-login-password"]);
+
+        execFileSync("docker", ["login", "--username", "AWS", "--password-stdin", repositoryUrl], { input: password });
+
+        const pushImage = (tag: string) => {
+            const remoteTag = `${gitHubContext.runNumber}-${tag}`;
+            execFileSync("docker", ["tag", tag, `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+            execFileSync("docker", ["push", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+        }
+
+        const pullImage = (tag: string) => {
+            const remoteTag = `${runNumber}-${tag}`;
+            execFileSync("docker", ["pull", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+            execFileSync("docker", ["tag", `${repositoryUrl}:${remoteTag}`, tag], { stdio: "inherit" });
+            execFileSync("docker", ["rmi", `${repositoryUrl}:${remoteTag}`], { stdio: "inherit" });
+        }
+
+        const tags = imageTag.split("\n").map((str) => str.trim()).filter(Boolean);
+        const uniqueTags = Array.from(new Set(tags));
+
+        uniqueTags.forEach(typeof runNumber !== "string" ? pushImage : pullImage)
+
+        execFileSync("docker", ["logout", repositoryUrl]);
+    } catch (err) {
+        if (err instanceof Error) setFailed(err);
+    }
+}
+
+main();
